e08d23da9bfaa46569a7b352a7373ceeef38d4c2
RaghulGopinathan <kraghul1776@gmail.com>
2023-12-23 13:19:16 +0530

    DifficultyUI done

--- app/src/main/java/com/example/kwizapp/DifficultyLevel.kt
+++ app/src/main/java/com/example/kwizapp/DifficultyLevel.kt
@@ -2,6 +2,9 @@ package com.example.kwizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.cardview.widget.CardView
 
 class DifficultyLevel : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
